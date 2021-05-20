697305b7530701fcba13db2eb511f61e72b38fb5
Marcinbloch1 <marcinb98@gmail.com>
2021-05-20 15:37:03 +0200

    initial project template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsComponent,
+    ChatComponent,
+    TeamsComponent,
+    TasksComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
