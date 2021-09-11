b609f60b9d35b127bc220ed67b514f71fee94006
Jarosław Pałasz <jaroslawpalasz.mail@gmail.com>
2021-09-11 22:35:03 +0200

    templates: Header component with 'chooseDate' button, Tasklist component as 2 tabs ('todo', 'done tasks'), task component with checkbox and 'see task' button, created fixed temporary of tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  
+  constructor(
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getCalendar(color: string) {
+    console.log("Choose date from calendar")
+    console.log(color)
+  }
 }
---
+++ src/app/task-mock.ts
@@ -0,0 +1,15 @@
+import { Task } from "./_models/Task"
+
+export const TASKS: Task[] = [
+    {   //personal task
+        id: 1,
+        deadline: "21.09.2021",
+        coachId: "coach-id",
+        playerId: "player-id",
+    },
+    {   //team task
+        id: 2,
+        deadline: "21.09.2021",
+        teamId: "team-id",
+    }
+]
