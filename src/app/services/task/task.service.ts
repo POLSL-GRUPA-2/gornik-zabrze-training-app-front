75ba55609e53c2b085860993d9d028954f02cbd0
Marcinbloch1 <marcinb98@gmail.com>
2021-09-15 12:22:25 +0200

    fixed merge

--- src/app/services/task/task.service.ts
+++ src/app/services/task/task.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 
 //importing observables - design pattern to handle passing messages from server -
 //- don't need 'of' after proper backend requests
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs'
 import { environment } from '../../../environments/environment'
 //importing task interface
 import { Task } from 'src/app/_models/Task'
@@ -20,23 +20,18 @@ export class TaskService {
   private currentUserUrl = environment.apiUrl + '/account'
   private playerUrl = environment.apiUrl + '/player'
 
-
-  private tasksUrl = environment.apiUrl + '/personal_task';
-  private currentUserUrl = environment.apiUrl + '/account';
-  private playerUrl = environment.apiUrl + '/player';
-
-  userId!: string;
+  userId!: string
   defaultTask!: Task
 
   //private http:HttpClient in a constructor argument
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //behavior subject holding current value of message
   private taskSource = new BehaviorSubject<Task>(this.defaultTask)
   //observable used by components
-  currentTask = this.taskSource.asObservable();
+  currentTask = this.taskSource.asObservable()
   //function changing current value of behavior subject
-  changeTask(message: Task){
+  changeTask(message: Task) {
     this.taskSource.next(message)
   }
 
@@ -74,9 +69,9 @@ export class TaskService {
 
   //PLACEHOLDER
   //getTasks(): Observable<Task[]> {
-    //placeholder
+  //placeholder
   //  const tasks = of(TASKS)
   //  return tasks
-    // return this.http.get<Task[]>(this.apiUrl)
+  // return this.http.get<Task[]>(this.apiUrl)
   //}
 }
