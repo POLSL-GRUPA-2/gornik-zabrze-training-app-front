7ec29938cea067a9cc6b074562d067111bf19ab6
Marcinbloch1 <marcinb98@gmail.com>
2021-09-15 14:57:09 +0200

    fixed showing id and deadline in task-dialog & better styling of tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,48 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { TaskDialogService } from 'src/app/services/task-dialog/task-dialog.service';
+import { Component, OnInit, Input } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { TaskDialogService } from 'src/app/services/task-dialog/task-dialog.service'
 
 // import task interface
-import { Task } from 'src/app/_models/Task';
-import { TaskDialogComponent } from '../task-dialog/task-dialog.component';
+import { Task } from 'src/app/_models/Task'
+import { TaskDialogComponent } from '../task-dialog/task-dialog.component'
 
 // task also contains checkbox
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent implements OnInit {
   //TODO: task undefined
-   @Input() task!: Task
-   @Input()
-  checked!: boolean;
+  @Input() task!: Task
+  @Input()
+  checked!: boolean
 
   //message used by service
-  message!: string;
+  message!: string
+  taskId!: number
+  deadline!: string
 
   //service used in constructor
-  constructor(public dialog: MatDialog, private data: TaskDialogService) { 
-  }
+  constructor(public dialog: MatDialog, private data: TaskDialogService) {}
 
   ngOnInit(): void {
-
     //subscribe to the current message observable and set its value to message variable
-    this.data.currentMessage.subscribe(message => this.message = message)
+    this.data.currentMessage.subscribe((message) => (this.message = message))
+    this.data.currentTaskId.subscribe((taskId) => (this.taskId = taskId))
+    this.data.currentDeadline.subscribe(
+      (deadline) => (this.deadline = deadline)
+    )
   }
 
   openDialog(): void {
-
-    //change value of subscribed message, when its executed - 
+    //change value of subscribed message, when its executed -
     //new data is automatically broadcast to all components subscribed to it
     this.data.changeMessage(this.task.description!)
+    this.data.changeTaskId(this.task.id!)
+    this.data.changeDeadline(this.task.task_date!)
 
-    const dialogRef = this.dialog.open(TaskDialogComponent);
+    const dialogRef = this.dialog.open(TaskDialogComponent)
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-    
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`)
+    })
   }
-  
 }
--- src/app/services/task-dialog/task-dialog.service.ts
+++ src/app/services/task-dialog/task-dialog.service.ts
@@ -1,21 +1,30 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskDialogService {
-
   //behavior subject holding current value of message
-  private messageSource = new BehaviorSubject<string>("default")
+  private messageSource = new BehaviorSubject<string>('default')
+  private taskIdSource = new BehaviorSubject<number>(1)
+  private deadlineSource = new BehaviorSubject<string>('')
 
   //observable used by components
-  currentMessage = this.messageSource.asObservable();
+  currentMessage = this.messageSource.asObservable()
+  currentTaskId = this.taskIdSource.asObservable()
+  currentDeadline = this.deadlineSource.asObservable()
 
-  constructor() { }
+  constructor() {}
 
   //function changing current value of behavior subject
-  changeMessage(message: string){
+  changeMessage(message: string) {
     this.messageSource.next(message)
+  }
+  changeTaskId(taskId: number) {
+    this.taskIdSource.next(taskId)
+  }
+  changeDeadline(deadline: string) {
+    this.deadlineSource.next(deadline)
   }
 }
