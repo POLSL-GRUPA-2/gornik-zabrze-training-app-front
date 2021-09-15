e1dca0fd644a0e0133611727b4b98b50e51a6a8b
Marcinbloch1 <marcinb98@gmail.com>
2021-09-16 00:55:10 +0200

    added heroku deploy

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url)
-  }
-  next()
-}
-
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.use(requireHTTPS)
-app.use(express.static('./dist/gornik-front'))
+app.use(express.static(__dirname + '/dist/gornikFront'))
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/gornik-front/' })
-)
+app.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'))
+})
 
-console.log('ASD')
-
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8000)
